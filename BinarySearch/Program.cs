43f2ef4602d0b3e26c93d894d2a11e8fa5195e2d
AlexSaskevich <saskevich.aleksey@gmail.com>
2024-03-17 18:21:08 +0300

    Add binary search

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace BinarySearch;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        int[] numbers = [-4, -3, 0, 1, 18, 523];
+
+        const int toFind = 198484;
+
+        int index = FindElementIndex(numbers, toFind);
+
+        Console.WriteLine($"{toFind} is present at index {index}");
+    }
+
+    private static int FindElementIndex(in int[] array, int toFind)
+    {
+        int left = 0;
+        int right = array.Length - 1;
+
+        while (left <= right)
+        {
+            int middle = (left + right) / 2;
+
+            if (array[middle] == toFind)
+            {
+                return middle;
+            }
+
+            if (array[middle] < toFind)
+            {
+                left = middle + 1;
+            }
+            else
+            {
+                right = middle - 1;
+            }
+        }
+
+        return -1;
+    }
+}
